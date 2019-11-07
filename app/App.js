5899d95a822587c0c91110f6687956732a0fc9bf
Shuto Nakano <xabdjx@gmail.com>
2019-11-07 15:37:08 +0900

    test

--- app/App.js
+++ app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-          <Text>Hello ReactNative!!</Text>
+          <Text>Hello World!!</Text>
     </View>
   );
 }
