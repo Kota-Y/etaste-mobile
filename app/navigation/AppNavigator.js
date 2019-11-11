5bd70708a19979ea914bcba1dfdd6124276ad1c8
Shuto Nakano <xabdjx@gmail.com>
2019-11-11 20:16:51 +0900

    Update Link from Setting

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
 
+import SettingsScreen from '../screens/SettingsScreen';
 import EditProfileScreen from '../screens/EditProfileScreen';
 import MainTabNavigator from './MainTabNavigator';
 
 export default createAppContainer(
-  createSwitchNavigator({
-    Main: MainTabNavigator
-  }),
-  /* createStackNavigator({
-    editProfile: EditProfileScreen
-  }) */
+  /* createSwitchNavigator({
+    Main: MainTabNavigator,
+  }), */
+  createStackNavigator({
+    list: SettingsScreen,
+    edit: EditProfileScreen
+  },
+  {
+    mode: 'card',
+  })
 );
--- app/screens/SettingsScreen.js
+++ app/screens/SettingsScreen.js
@@ -1,42 +1,80 @@
 import React from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+//import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-import SettingList from '../components/SettingList';
-import EditProfileScreen from '../screens/EditProfileScreen';
+
+const list = [
+  {
+    title: 'プロフィール編集',
+    id: 1
+  },
+  {
+    title: 'Q&A', 
+    id: 2
+  },
+  {
+    title: 'お問い合わせ',
+    id: 3
+  },
+  {
+    title: 'オープンソース',
+    id: 4
+  },
+  {
+    title: '利用規約',
+    id: 5
+  },
+  {
+    title: '特定商品取引法',
+    id: 6
+  },
+  {
+    title: 'プライバシーポリシー',
+    id: 7
+  },
+  {
+    title: 'バージョン情報',
+    id: 8
+  },
+  {
+    title: 'ログアウト',
+    id: 9
+  },
+  {
+    title: '退会について',
+    id: 10
+  }
+]
 
 export default class SettingsScreen extends React.Component {
 
-  push = (to) => {
-    const { navigation } = this.props
-    navigation.navigate(to)
-  }
-
   render(){
 
-    const Layout = createAppContainer(SettingStack);
-
     return(
-      <Layout/>
+      <View>
+        {/* 一覧表示 */}
+        <FlatList
+          data={list}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+              <ListItem
+                title={item.title}
+                bottomDivider
+                chevron
+                onPress={() => this.props.navigation.navigate('edit')}
+              />
+          )}
+        />
+      </View>
     );
   }
 
 }
 
 SettingsScreen.navigationOptions = {
-  title: '設定',
-};
-
-const SettingStack = createStackNavigator(
-  {
-    list: SettingList,
-    editProfile: EditProfileScreen,
-  },
-  {
-    initialRouteName: 'list'
-  }
-);
+  title:'設定',
+}
 
 
 const styles = StyleSheet.create({
